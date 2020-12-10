26ceb0e8d8cc9917069a3a4d7628751804b71d15
aimanyounises1 <aimanyounises.gmail.com>
2020-12-10 12:37:35 +0200

    added menu and category

--- app/src/main/java/com/example/sportify/MainActivity.java
+++ app/src/main/java/com/example/sportify/MainActivity.java
@@ -25,46 +25,47 @@ public class MainActivity extends AppCompatActivity {
     EditText pass_edit;
     FirebaseAuth auth = FirebaseAuth.getInstance();
     Button AdminButton;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // login activity
 
-        email_edit =  findViewById(R.id.email_edit);
-        pass_edit = findViewById(R.id.password) ;
-        sign_in_button =findViewById(R.id.signin);
+        email_edit = findViewById(R.id.email_edit);
+        pass_edit = findViewById(R.id.password);
+        sign_in_button = findViewById(R.id.signin);
         sign_up_button = findViewById(R.id.signup);
         AdminButton = findViewById(R.id.admin);
-        sign_in_button.setOnClickListener(view ->{
+        sign_in_button.setOnClickListener(view -> {
             String email_text = email_edit.getText().toString().trim();
             String password_text = pass_edit.getText().toString().trim();
 
-            auth.signInWithEmailAndPassword(email_text,password_text).addOnCompleteListener(login ->{
-                if(login.isSuccessful()) {
+            auth.signInWithEmailAndPassword(email_text, password_text).addOnCompleteListener(login -> {
+                if (login.isSuccessful()) {
                     Intent myIntent = new Intent(getApplicationContext(), CategoryActivity.class);
                     startActivity(myIntent);
                     finish();
-                }
-                else {
-                    Toast.makeText(getApplicationContext(),"Wrong email or password",Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong email or password", Toast.LENGTH_LONG).show();
                 }
 
             });
         });
-        
+
         sign_up_button.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this,SignUpActivity.class);
+            Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
             startActivity(intent);
         });
 
     }
 
     public void contact_us(View view) {
-        Intent intent= new Intent(Intent.ACTION_DIAL, Uri.parse("tel: 0524472633"));
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel: 0524472633"));
         startActivity(intent);
     }
     // in xml file of mainactivity we had attribute called onClick= "admin_login" he knows that he will call this method
     // in MainActivity class
     public void admin_login(View view) {
+
         /*
         let's validate if the desired user is really an admin
          */
@@ -75,7 +76,8 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for(DataSnapshot snap:snapshot.getChildren())
                 {
-                    if(snap.getValue().equals(email_edit.getText().toString()))
+
+                    if(snap.child("Email").getValue().equals(email_edit.getText().toString()))
                     {
 
                         auth.signInWithEmailAndPassword(email_edit.getText().toString(),pass_edit.getText().toString()).addOnCompleteListener(login -> {
@@ -89,10 +91,11 @@ public class MainActivity extends AppCompatActivity {
 
                         });
                     }
-                    else
-                    {
-                        Toast.makeText(MainActivity.this, "You're not an admin !", Toast.LENGTH_SHORT).show();
-                    }
+//                    else
+//                    {
+//                        if(is_admin.eq)
+//                        Toast.makeText(MainActivity.this, "You're not an admin !", Toast.LENGTH_SHORT).show();
+//                    }
                 }
             }
 
@@ -103,5 +106,4 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
 }
